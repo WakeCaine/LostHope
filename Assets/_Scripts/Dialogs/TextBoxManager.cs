9940bc53c885ff51db5f35a1756b5c9c9fec08a7
WakeCaine <dako123d@gmail.com>
2016-10-19 22:01:16 +0200

    Initial commit

---
+++ Assets/_Scripts/Dialogs/TextBoxManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+	public GameObject textBox;
+	public Text dialogText;
+	public TextAsset textFile;
+	public string[] textLines;
+
+	public int currentLine = 0;
+	public int endAtLine = 0;
+
+	public Player player;
+
+	private GameObject camera;
+
+	private bool isZoomed = false;
+
+	private bool isActive = false;
+	// Use this for initialization
+	void Start ()
+	{
+		player = FindObjectOfType<Player> ();
+		camera = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!isActive) {
+			return;
+		}
+
+		if (isZoomed == true) {
+			Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, 40, Time.deltaTime * 5.0f);
+		} else if (isZoomed == false) {
+			Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, 84.4f, Time.deltaTime * 5.0f);
+		}
+
+		dialogText.text = textLines [currentLine];
+
+		if (Input.GetKeyDown (KeyCode.E) && currentLine < endAtLine) {
+			currentLine += 1;
+		} else if (Input.GetKeyDown (KeyCode.E) && currentLine >= endAtLine) {
+			textBox.SetActive (false);
+			DisableTextBox ();
+			isZoomed = false;
+		}
+	}
+
+	public void EnableTextBox ()
+	{
+		textBox.SetActive (true);
+		player.canMove = false;
+	}
+
+	public void DisableTextBox ()
+	{
+		textBox.SetActive (false);
+		player.canMove = true;
+	}
+
+	public void ReloadScript (TextAsset inputText)
+	{
+		if (inputText != null) {
+			//camera.transform = 
+			isZoomed = true;
+			textLines = new string[1];
+			textFile = inputText;
+			textLines = (inputText.text.Split ('\n'));
+			isActive = true;
+
+			if (endAtLine == 0) {
+				endAtLine = textLines.Length - 1;
+			}
+
+			if (isActive) {
+				EnableTextBox ();
+			} else {
+				DisableTextBox ();
+			}
+		}
+
+
+	}
+}
---
+++ Assets/_Scripts/UnitControllers/Player.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MovingObject
+{
+	public Text foodText;
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 1f;
+
+	public AudioClip moveSound1;
+	public AudioClip moveSound2;
+	public AudioClip eatSound1;
+	public AudioClip eatSound2;
+	public AudioClip drinkSound1;
+	public AudioClip drinkSound2;
+	public AudioClip gameOverSound;
+
+	public bool canMove = true;
+
+	private Animator animator;
+	private int food;
+
+
+	// Use this for initialization
+	protected override void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		food = GameManager.instance.playerFoodPoints;
+		foodText.text = "Food: " + food;
+
+		base.Start ();
+	}
+
+	private void OnDisable ()
+	{
+		GameManager.instance.playerFoodPoints = food;
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!GameManager.instance.playersTurn)
+			return;
+		else if (!canMove)
+			return;
+
+		int horizontal = 0;
+		int vertical = 0;
+
+		horizontal = (int)(Input.GetAxisRaw ("Horizontal"));
+		vertical = (int)(Input.GetAxisRaw ("Vertical"));
+
+		if (horizontal != 0)
+			vertical = 0;
+
+		if (horizontal != 0 || vertical != 0) {
+			AttemptMove<Wall> (horizontal, vertical);
+		}
+	}
+
+	protected override void OnCantMove<T> (T component)
+	{
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall (wallDamage);
+		animator.SetTrigger ("playerChop");
+	}
+
+	private void Restart ()
+	{
+		SceneManager.LoadScene (0);
+	}
+
+	public void LooseFood (int loss)
+	{
+		animator.SetTrigger ("playerHit");
+		food -= loss;
+		foodText.text = "-" + loss + " Food: " + food;
+		CheckIfGameOver ();
+	}
+
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Exit") {
+			Invoke ("Restart", restartLevelDelay);
+			enabled = false;
+		} else if (other.tag == "Food") {
+			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + " Food: " + food;
+			SoundManager.instance.RandomizeSfx (eatSound1, eatSound2);
+			other.gameObject.SetActive (false);
+		} else if (other.tag == "Soda") {
+			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + " Food: " + food;
+			SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
+			other.gameObject.SetActive (false);
+		} else if (other.tag == "NPC") {
+			//other.gameObject.transform.position = Vector3.MoveTowards (other.transform.position, new Vector3 (other.transform.position.x + 1, other.transform.position.y, other.transform.position.z), 1f);
+		}
+	}
+
+	protected override void AttemptMove<T> (int xDir, int yDir)
+	{
+		food--;
+		foodText.text = "Food: " + food;
+
+		base.AttemptMove<T> (xDir, yDir);
+		if (xDir < 0) {
+			transform.localRotation = Quaternion.Euler (0, 180, 0);
+		} else if (xDir > 0) {
+			transform.localRotation = Quaternion.Euler (0, 0, 0);
+		}
+
+		RaycastHit2D hit;
+		if (Move (xDir, yDir, out hit)) {
+			SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
+		}
+
+		CheckIfGameOver ();
+
+		GameManager.instance.playersTurn = false;
+	}
+
+	private void CheckIfGameOver ()
+	{
+		if (food <= 0) {
+			SoundManager.instance.musicSource.Stop ();
+			SoundManager.instance.RandomizeSfx (gameOverSound);
+			GameManager.instance.GameOver ();
+		}
+	}
+}
